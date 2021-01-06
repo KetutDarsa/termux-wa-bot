f7a45d8e18cdf4e0513e9c4287a5e7172a4c97c5
Jaya_github <74484663+KetutDarsa@users.noreply.github.com>
2021-01-07 06:22:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,25 @@ const {
 } = require('@adiwajshing/baileys')
 const { color, bgcolor } = require('./lib/color')
 const { help } = require('./src/help')
+const { bahasa } = require('./src/bahasa.js')
+const { negara } = require('./src/kodenegara.js')
 const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
 const { fetchJson } = require('./lib/fetcher')
 const { recognize } = require('./lib/ocr')
+const { virtex } = require('./src/virtex.js')
 const fs = require('fs')
 const moment = require('moment-timezone')
 const { exec } = require('child_process')
 const kagApi = require('@kagchi/kag-api')
 const fetch = require('node-fetch')
-const tiktod = require('tiktok-scraper')
 const ffmpeg = require('fluent-ffmpeg')
 const { removeBackgroundFromImageFile } = require('remove.bg')
 const imgbb = require('imgbb-uploader')
-const lolis = require('lolis.life')
-const loli = new lolis()
 const welkom = JSON.parse(fs.readFileSync('./src/welkom.json'))
 const nsfw = JSON.parse(fs.readFileSync('./src/nsfw.json'))
 const samih = JSON.parse(fs.readFileSync('./src/simi.json'))
-prefix = '$'
+const speed = require('performance-now')
+prefix = $
 blocked = []
 
 function kyun(seconds){
